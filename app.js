a01c0bcda5f13d2bd04e9c106ef077aa3c4ca77d
Danial <danialbarbodjalali@gmail.com>
2024-10-21 21:18:07 +0330

    Add app.js
    Selecting element

---
+++ app.js
@@ -0,0 +1,6 @@
+let blogTitle = document.querySelector("#title");
+let blogBody = document.querySelector("#body");
+let commentDiv = document.querySelector("#commetnDiv");
+let name = document.querySelector("#name");
+let comment = document.querySelector("#comment");
+let submit = document.querySelector("#submit");
